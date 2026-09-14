b32490e88ee16e80f1be5b76e45b4fe5e9f2ba26
Kartik <whonalekartik11@gmail.com>
2026-09-14 12:47:23 +0530

    vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Analytics } from '@vercel/analytics/next';
+import { Analytics } from "@vercel/analytics/react";
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
